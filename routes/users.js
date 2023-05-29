73c33ce37fb1b65bb48c52afd3f74faec8b811d4
Nurmatov Saidamirxon <99072823+NurmatovAmeer@users.noreply.github.com>
2023-05-29 21:45:13 +0500

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,12 @@ import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 // UPDATE
-router.put("/:id", verifyUser, updateUser);
+router.put("/:id", updateUser);
 // DELETE
-router.delete("/:id", verifyUser, deleteUser);
+router.delete("/:id", deleteUser);
 // GET ONE
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", getUser);
 // GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", getUsers);
 
 export default router;
