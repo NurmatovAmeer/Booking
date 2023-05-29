d5e0ac76c5e3236b81f7c7877cd42521be148888
Nurmatov Saidamirxon <99072823+NurmatovAmeer@users.noreply.github.com>
2023-05-29 21:05:14 +0500

    Add files via upload

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://salty-cliffs-19039.herokuapp.com/api";
-export const API_URL = "http://localhost:8800/api";
+export const API_URL = "https://clover-booking.onrender.com/api";
